23c6a4c92626e951bfd32d3ef15cf3dcb47affe8
Dave Jacobsen <dave@littlepunk.net>
2020-07-02 21:17:23 +1200

    latest

--- autohome-build.sh
+++ autohome-build.sh
@@ -5,10 +5,18 @@ HISTCONTROL=ignoreboth
 #chmod +x autohome-build.sh
 #./autohome-build.sh
 #curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+
+# Install node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+source ~/.bashrc
+nvm install 12
+
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install -y samba samba-common-bin
+
+# Setup up CIFS/SAMBA share
+sudo apt install -y samba samba-common-bin librrds-perl rrdtool
 sudo tee -a /etc/samba/smb.conf > /dev/null <<EOT
 [Autohome]
 comment=AutoHome Share
@@ -20,19 +28,25 @@ create mask=0777
 directory mask=0777
 public=no
 EOT
+# Will pause and ask for a password
 sudo smbpasswd -a pi
 sudo service smbd restart
-echo 'Your IP is:'
-ifconfig | grep 192
+
+# Show IP
+IFS=' '
+read -ra ADDR <<< $(ifconfig | grep 192)
+echo 'Your ip is: ' ${ADDR[1]}
+
+# Tidy up
 sudo apt autoremove -y
 #sudo apt-get install -y node.js
 #sudo apt-get install -y gcc g++ make screen
-sudo npm install -g nodemon
+npm install -g nodemon node-gyp
+
+# should replace this line with npm install which will use package.json
 npm install serialport tplink-smarthome-api colors socket.io express body-parser tuyapi util
 
-# RRD Tool
-sudo apt-get install -y librrds-perl rrdtool
 # May also need to do:
-sudo npm install -g node-gyp
+#sudo npm install -g node-gyp
 # ?? git clone https://github.com/Orion98MC/node_rrd.git
 
