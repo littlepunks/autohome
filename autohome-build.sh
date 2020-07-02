06aa545e70cb23fd23287562b22ad9e6b5ecdddb
Dave Jacobsen <dave@littlepunk.net>
2020-07-03 00:04:24 +1200

    test

--- autohome-build.sh
+++ autohome-build.sh
@@ -5,43 +5,49 @@ HISTCONTROL=ignoreboth
 #./autohome-build.sh
 
 # Install node version manager
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source ~/.bashrc
-nvm install 12
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#source ~/.bashrc
+#nvm install 12
+wget https://nodejs.org/dist/v12.18.2/node-v12.18.2-linux-arm64.tar.gz
+tar -xzf node-v12.18.2-linux-arm64.tar.gz
+cd node-v12.18.2-linux-arm64/
+sudo cp -R * /usr/local/
 
-sudo apt update
-sudo apt full-upgrade -y
+
+
+#sudo apt update
+#sudo apt full-upgrade -y
 
 # Setup up CIFS/SAMBA share
-sudo apt install -y samba samba-common-bin librrds-perl rrdtool
-sudo tee -a /etc/samba/smb.conf > /dev/null <<EOT
-[Autohome]
-comment=AutoHome Share
-path=/home/pi/autohome
-browsable=yes
-writeable=yes
-only guest=no
-create mask=0777
-directory mask=0777
-public=no
-EOT
+#sudo DEBIAN_FRONTEND=noninteractive apt install -y samba samba-common-bin librrds-perl rrdtool
+#sudo tee -a /etc/samba/smb.conf > /dev/null <<EOT
+#[Autohome]
+#comment=AutoHome Share
+#path=/home/pi/autohome
+#browsable=yes
+# writeable=yes
+# only guest=no
+# create mask=0777
+# directory mask=0777
+# public=no
+# EOT
 # Will pause and ask for a password
-sudo smbpasswd -a pi
-sudo service smbd restart
+# sudo smbpasswd -a pi
+# sudo service smbd restart
 
 # Show IP
-IFS=' '
-read -ra ADDR <<< $(ifconfig | grep 192)
-echo 'Your ip is: ' ${ADDR[1]}
+# IFS=' '
+# read -ra ADDR <<< $(ifconfig | grep 192)
+# echo 'Your ip is: ' ${ADDR[1]}
 
 # Tidy up
-sudo apt autoremove -y
+# sudo apt autoremove -y
 #sudo apt-get install -y node.js
 #sudo apt-get install -y gcc g++ make screen
-npm install -g nodemon node-gyp
+# npm install -g nodemon node-gyp
 
 # should replace this line with npm install which will use package.json
-npm install serialport tplink-smarthome-api colors socket.io express body-parser tuyapi util
+npm install serialport tplink-smarthome-api colors socket.io express body-parser tuyapi request util
 
 # May also need to do:
 #sudo npm install -g node-gyp
